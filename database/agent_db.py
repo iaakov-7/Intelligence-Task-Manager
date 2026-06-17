d26a268739e4cbcc41827e19814646893ba0fd2d
yaakovizel <z7685727@gmail.com>
2026-06-17 16:00:16 +0300

    finish agent class

--- database/agent_db.py
+++ database/agent_db.py
@@ -1,4 +1,5 @@
 from db_connection import db
+from db_exceptions import IdCannotChengedError
 
 class AgentDB:
     def __init__(self):
@@ -20,8 +21,94 @@ class AgentDB:
     
     def get_all_agents(self):
         conn = self.db.get_connection()
-        cursor = conn.cursor()
-        pass
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM agents")
+        agents = cursor.fetchall()
+        cursor.close()
+        return agents
     
+    def get_agent_by_id(self,id:int):
+        conn = self.db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM agents WHERE id= %s",(id,))
+        agent = cursor.fetchone()
+        cursor.close()
+        return agent
+    
+    def update_agent(self,id:int, data:dict):
+        if "id" in data:
+            raise IdCannotChengedError
+        conn = self.db.get_connection()
+        cursor = conn.cursor()
+        set_columns = [f"{key}=%s" for key in data.keys()]
+        set_clause = ", ".join(set_columns) 
+        query = f"""UPDATE agents
+                SET {set_clause}
+                WHERE id = %s"""
+        values = list(data.values())
+        cursor.execute(query,values+[id])
+        conn.commit()
+        updated = cursor.rowcount > 0
+        cursor.close()
+        if updated:
+            return f"Agent {id} updated successfully"
+        return "Update failed " 
+
+    def deactivate_agent(self,id:int):
+        conn = self.db.get_connection()
+        cursor = conn.cursor() 
+        cursor.execute("UPDATE agents SET is_active=FALSE WHERE id=%s",(id,)) 
+        conn.commit()
+        updated = cursor.rowcount > 0
+        cursor.close()
+        if updated:
+            return f"Agent {id} deactivated successfully"
+        return "Deactivate failed"
+
+    def increment_completed(self,id:int):         
+        conn = self.db.get_connection()
+        cursor = conn.cursor() 
+        cursor.execute("UPDATE agents SET completed_missions=completed_missions+1 WHERE id=%s",(id,)) 
+        conn.commit()
+        updated = cursor.rowcount > 0
+        cursor.close()
+        if updated:
+            return f"Agent {id} updated successfully"
+        return "Update failed "
+    
+    def increment_failed(self,id:int):
+        conn = self.db.get_connection()
+        cursor = conn.cursor() 
+        cursor.execute("UPDATE agents SET failed_missions=failed_missions+1 WHERE id=%s",(id,)) 
+        conn.commit()
+        updated = cursor.rowcount > 0
+        cursor.close()
+        if updated:
+            return f"Agent {id} updated successfully"
+        return "Update failed"    
+
+    def get_agent_performance(self,id:int):
+        conn = self.db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT completed_missions AS completed FROM agents WHERE id=%s",(id,))
+        completed_dict = cursor.fetchone()
+        cursor.execute("SELECT failed_missions AS failed FROM agents WHERE id=%s",(id,))
+        failed_dict = cursor.fetchone()
+        cursor.close() 
+        total_dict = {"total":completed_dict["completed"] + failed_dict["failed"] } 
+        try:
+            success_rate_dict = {"success_rate":completed_dict["completed"] / total_dict["total"] * 100}
+        except ZeroDivisionError:
+             success_rate_dict = {"success_rate":0}   
+        return completed_dict | failed_dict | total_dict | success_rate_dict
+    
+    def count_active_agents(self):
+        conn = self.db.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT COUNT(*) AS active_agents FROM agents WHERE is_active=TRUE ")
+        active_agents = cursor.fetchone()
+        cursor.close()
+        return active_agents
+
 db_agent = AgentDB()
-db.create_database()
+
