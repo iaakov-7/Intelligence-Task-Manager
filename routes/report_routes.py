bd1a2dd6db086459e6a7b61e9799759922d2e7c1
yaakovizel <z7685727@gmail.com>
2026-06-18 15:53:10 +0300

    added report routes

---
+++ routes/report_routes.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, HTTPException
+import mysql.connector
+from database.agent_db import db_agent
+from database.mission_db import db_mission
+
+router = APIRouter(tags=["Reports"])
+
+@router.get("/summary")
+def get_general_report():
+    general = { 
+  "active_agents_count": db_agent.count_active_agents().get("active_agents"), 
+  "total_missions": db_mission.count_all_missions().get("total_missions"),
+  "open_missions": db_mission.count_open_missions().get("total_open_missions"),
+  "completed_missions": db_mission.count_by_status("COMPLETED").get("total"), 
+  "failed_missions": db_mission.count_by_status("FAILED").get("total"), 
+  "critical_missions": db_mission.count_critical_missions().get("total_critical_missions")
+    }
+    return general
+
+@router.get("/missions-by-status")
+def get_by_status():
+    report = { 
+  "open": db_mission.count_open_missions().get("total_open_missions"), 
+  "in_progress": db_mission.count_by_status("IN_PROGRESS").get("total"), 
+  "completed":db_mission.count_by_status("COMPLETED").get("total"), 
+  "failed": db_mission.count_by_status("FAILED").get("total"),
+   "cancelled":db_mission.count_by_status("CANCELLED").get("total") }
+    return report
+
+@router.get("/top-agent")
+def get_top_agent():
+    if len(db_agent.get_all_agents()) == 0:
+        raise HTTPException(404,"There are no agents in db")
+    top = db_mission.get_top_agent()
+    return top
